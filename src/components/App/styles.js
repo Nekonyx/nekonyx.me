82008ddd9060cb8347e463196d345cd9b82c9fc5
Leonard Timofeev <nekonyx@mail.ru>
2018-06-22 03:16:27 +0600

    0.7.3. Now responsive.

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -5,12 +5,18 @@
 
 import styled from 'styled-components';
 
+export const onMobile = '@media (min-width: 320px) and (max-width: 1025px)';
+
 export const Wrapper = styled.div`
   display: flex;
   height: 100%;
   padding: 8em;
   align-items: flex-end;
   justify-content: space-between;
+
+  ${onMobile} {
+    padding: 3em 1.4em;
+  }
 `;
 
 export const Container = styled.div`
@@ -26,6 +32,11 @@ export const Container = styled.div`
     animation: fadeInDown ease-in-out .6s;
     font-size: 3em;
     letter-spacing: 11.5px;
+
+    ${onMobile} {
+      font-size: 2em;
+      letter-spacing: 6px;
+    }
   }
 
   h2 {
@@ -33,6 +44,10 @@ export const Container = styled.div`
     font-weight: 400;
     margin-top: 1em;
     font-size: 1em;
+
+    ${onMobile} {
+      font-size: .8em;
+    }
   }
 
   span {
